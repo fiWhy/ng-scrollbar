fc7dd9df9e14649112516395dd028f254b6f78de
Denis <fiwhy@mail.ru>
2017-08-29 20:42:41 +0300

    Rewrote all data

--- src/ng-scrollbar.js
+++ src/ng-scrollbar.js
@@ -12,319 +12,144 @@ angular.module('ngScrollbar', []).directive('ngScrollbar', [
       scope: {
         'showYScrollbar': '=?isBarShown',
         'touchEventWidth': '=',
-        'hideScrollOnOut': '='
+        'hideScrollOnOut': '=',
+        'hideTimeout': '=',
       },
       link: function (scope, element, attrs) {
+        var raf = window.requestAnimationFrame || window.setImmediate || function (c) { return setTimeout(c, 0); };
+        var documentBody = angular.element(document.body),
+          scrollRatio;
+        var container = element,
+          wrapper = angular.element(element.children()[0]),
+          content = angular.element(wrapper.children()[0]),
+          bar = wrapper.next();
 
-        var mainElm, transculdedContainer, tools, thumb, thumbLine, track;
+        var touchEndTimeout;
 
-        var enableTouchEvent = function () {
-          var touchEventWidth = scope.touchEventWidth || 768;
-          return window.innerWidth <= touchEventWidth;
-        };
-
-        var hideScrollEnabled = function () {
-          return scope.hideScrollOnOut && enableTouchEvent();
+        function enableMobileDeeds() {
+          return window.innerWidth <= (scope.touchEventWidth || 768);
         }
 
-        var calculateYOffset = function (offsetY) {
-          return enableTouchEvent() ? -offsetY : offsetY;
+        function enableHideScroll() {
+          return scope.hideScrollOnOut;
         }
 
-        var flags = {
-          bottom: attrs.hasOwnProperty('bottom')
-        };
+        function notifyShow() {
+          $timeout.cancel(touchEndTimeout);
+          scope.showYScrollbar = true;
+          scope.$digest();
+          scope.$emit("scrollbar.show");
+        }
 
-        var win = angular.element($window);
-
-        var hasAddEventListener = !!win[0].addEventListener;
-        var hasRemoveEventListener = !!win[0].removeEventListener;
-
-        // Elements
-        var dragger = {
-          top: 0
-        }, page = {
-          top: 0
-        };
-
-        // Styles
-        var scrollboxStyle, draggerStyle, draggerLineStyle, pageStyle;
-
-        var calcStyles = function () {
-          scrollboxStyle = {
-            position: 'relative',
-            overflow: 'hidden',
-            'max-width': '100%',
-            height: '100%'
-          };
-
-          if (page.height) {
-            scrollboxStyle.height = page.height + 'px';
-          }
-
-          draggerStyle = {
-            position: 'absolute',
-            height: dragger.height + 'px',
-            top: dragger.top + 'px'
-          };
-
-          draggerLineStyle = {
-            position: 'relative',
-            'line-height': dragger.height + 'px'
-          };
-
-          pageStyle = {
-            position: 'relative',
-            top: page.top + 'px',
-            overflow: 'hidden'
-          };
-        };
-
-        var redraw = function () {
-          thumb.css('top', dragger.top + 'px');
-          var draggerOffset = dragger.top / page.height;
-          page.top = -Math.round(page.scrollHeight * draggerOffset);
-          transculdedContainer.css('top', page.top + 'px');
-        };
-
-        var trackClick = function (event) {
-          var offsetY = event.hasOwnProperty('offsetY') ? event.offsetY : event.layerY;
-          var newTop = Math.max(0, Math.min(parseInt(dragger.trackHeight, 10) - parseInt(dragger.height, 10), offsetY));
-          dragger.top = newTop;
-          redraw();
-
-          event.stopPropagation();
-        };
-
-        var wheelHandler = function (event) {
-
-          var wheelSpeed = 40;
-
-          // Mousewheel speed normalization approach adopted from
-          // http://stackoverflow.com/a/13650579/1427418
-          var o = event, d = o.detail, w = o.wheelDelta, n = 225, n1 = n - 1;
-
-          // Normalize delta
-          d = d ? w && (f = w / d) ? d / f : -d / 1.35 : w / 120;
-          // Quadratic scale if |d| > 1
-          d = d < 1 ? d < -1 ? (-Math.pow(d, 2) - n1) / n : d : (Math.pow(d, 2) + n1) / n;
-          // Delta *should* not be greater than 2...
-          event.delta = Math.min(Math.max(d / 2, -1), 1);
-
-          event.delta = event.delta * wheelSpeed;
-
-          dragger.top = Math.max(0, Math.min(parseInt(page.height, 10) - parseInt(dragger.height, 10), parseInt(dragger.top, 10) - event.delta));
-          redraw();
-
-          if (!!event.preventDefault) {
-            event.preventDefault();
-          } else {
-            return false;
-          }
-        };
-
-        var lastOffsetY = 0;
-
-        var thumbDrag = function (event, offsetX, offsetY) {
-          dragger.top = Math.max(0, Math.min(parseInt(dragger.trackHeight, 10) - parseInt(dragger.height, 10), calculateYOffset(offsetY)));
-          event.stopPropagation();
-        };
-
-        var dragHandler = function (event) {
-          var newOffsetX = 0;
-          var newOffsetY = event.pageY - thumb[0].scrollTop - lastOffsetY;
-          thumbDrag(event, newOffsetX, newOffsetY);
-          redraw();
-        };
-
-        var _mouseUp = function (event) {
-          win.off('mousemove', dragHandler);
-          win.off('mouseup', _mouseUp);
-          event.stopPropagation();
-        };
-
-        var _touchDragHandler = function (event) {
-          var newOffsetX = 0;
-          var changedTouches = event.changedTouches ? event.changedTouches : event.originalEvent.changedTouches;
-          var newOffsetY = changedTouches[0].pageY - parseInt(thumb.css("top") || 0) - lastOffsetY;
-          thumbDrag(event, newOffsetX, newOffsetY);
-          redraw();
-        };
-
-        var _touchEnd = function (event) {
-          win.off('touchmove', _touchDragHandler);
-          win.off('touchend', _touchEnd);
-          event.stopPropagation();
-        };
-
-        var registerEvent = function (elm, eventName) {
-          var event = eventName || 'mousewheel';
-          if (hasAddEventListener) {
-            elm.addEventListener(event, wheelHandler, false);
-          } else {
-            elm.attachEvent('on' + event, wheelHandler);
-          }
-        };
-
-        var removeEvent = function (elm, eventName) {
-          var event = eventName || 'mousewheel';
-          if (hasRemoveEventListener) {
-            elm.removeEventListener(event, wheelHandler, false);
-          } else {
-            elm.detachEvent('on' + event, wheelHandler);
-          }
-        };
-
-        var buildScrollbar = function (rollToBottom) {
-
-          rollToBottom = flags.bottom || rollToBottom;
-          mainElm = angular.element(element.children()[0]);
-          transculdedContainer = angular.element(mainElm.children()[0]);
-          tools = angular.element(mainElm.children()[1]);
-          thumb = angular.element(angular.element(tools.children()[0]).children()[0]);
-          thumbLine = angular.element(thumb.children()[0]);
-          track = angular.element(angular.element(tools.children()[0]).children()[1]);
-
-          page.height = element[0].offsetHeight;
-          page.scrollHeight = transculdedContainer[0].scrollHeight;
-
-          var emitShow = function () {
-            $timeout(function () {
-              scope.showYScrollbar = true;
-              scope.$emit('scrollbar.show');
-            });
-          }
-
-          var emitHide = function (e) {
-            $timeout(function () {
+        function notifyHide() {
+          var hideTimeout = scope.hideTimeout || 1000;
+          if (enableHideScroll()) {
+            touchEndTimeout = $timeout(function () {
               scope.showYScrollbar = false;
-              scope.$emit('scrollbar.hide');
-            });
+              scope.$emit("scrollbar.hide");
+            })
           }
+        }
 
-          if (page.height < page.scrollHeight) {
-            if (!hideScrollEnabled()) {
-              emitShow();
-            }
-            // Calculate the dragger height
-            dragger.height = Math.round(page.height / page.scrollHeight * page.height);
-            dragger.trackHeight = page.height;
+        // Mouse drag handler
+        function dragDealer(el) {
+          var lastPageY;
 
-            // update the transcluded content style and clear the parent's
-            calcStyles();
-            element.css({ overflow: 'hidden' });
-            mainElm.css(scrollboxStyle);
-            transculdedContainer.css(pageStyle);
-            thumb.css(draggerStyle);
-            thumbLine.css(draggerLineStyle);
+          el.on('mousedown', function (e) {
+            lastPageY = e.pageY;
+            el.addClass('ss-grabbed');
+            documentBody.addClass('ss-grabbed');
 
-            // Bind scroll bar events
-            track.bind('click', trackClick);
-
-            // Handle mousewheel
-            registerEvent(transculdedContainer[0]);
-
-            // Drag the scroller with the mouse
-            thumb.on('mousedown', function (event) {
-              lastOffsetY = event.pageY - thumb[0].offsetTop;
-              win.on('mouseup', _mouseUp);
-              win.on('mousemove', dragHandler);
-              event.preventDefault();
-            });
-
-            scope.showHideScroller = function (event, flag) {
-              var method = !scope.hideScrollOnOut ? function () { } : flag ? emitShow : emitHide;
-              method(event);
-            }
-
-            // Drag the scroller by touch
-            var touchStartCallback = function (event) {
-              var changedTouches = event.changedTouches ? event.changedTouches : event.originalEvent.changedTouches;
-              lastOffsetY = changedTouches[0].pageY - thumb[0].offsetTop;
-              win.on('touchend', _touchEnd);
-              win.on('touchmove', _touchDragHandler);
-              if (hideScrollEnabled()) {
-                scope.showHideScroller(event, true);
-              }
-              event.preventDefault();
-            };
-
-            var touchEndCallback = function (event) {
-              if (hideScrollEnabled()) {
-                scope.showHideScroller(event, false);
-              }
-            }
-            thumb.on('touchstart', touchStartCallback);
-            transculdedContainer.on('touchstart', function (e) {
-              touchStartCallback(e);
-            });
-            transculdedContainer.on('touchend', touchEndCallback);
-
-            if (rollToBottom) {
-              flags.bottom = false;
-              dragger.top = parseInt(page.height, 10) - parseInt(dragger.height, 10);
-            } else {
-              dragger.top = Math.max(0, Math.min(parseInt(page.height, 10) - parseInt(dragger.height, 10), parseInt(dragger.top, 10)));
-            }
-
-            redraw();
-          } else {
-            emitHide();
-
-            thumb.off('mousedown');
-            removeEvent(transculdedContainer[0]);
-            transculdedContainer.attr('style', 'position:relative;top:0'); // little hack to remove other inline styles
-            mainElm.css({ height: '100%' });
-          }
-        };
-
-        var rebuildTimer;
-
-        var rebuild = function (e, data) {
-          /* jshint -W116 */
-          if (rebuildTimer != null) {
-            clearTimeout(rebuildTimer);
-          }
-          /* jshint +W116 */
-          var rollToBottom = !!data && !!data.rollToBottom;
-          rebuildTimer = setTimeout(function () {
-            page.height = null;
-            buildScrollbar(rollToBottom);
-            if (!scope.$$phase) {
-              scope.$digest();
-            }
-            // update parent for flag update
-            if (!scope.$parent.$$phase) {
-              scope.$parent.$digest();
-            }
-          }, 72);
-        };
-
-        buildScrollbar();
-
-        if (!!attrs.rebuildOn) {
-          attrs.rebuildOn.split(' ').forEach(function (eventName) {
-            scope.$on(eventName, rebuild);
+            documentBody.on('mousemove', drag);
+            documentBody.on('mouseup', stop);
+            return false;
           });
+          content.on("touchstart", function () {
+            notifyShow();
+          });
+
+          content.on("touchend", function () {
+            notifyHide();
+          });
+
+          content.on("mouseleave", function () {
+            onMouseLeave();
+          })
+
+          function drag(e) {
+            notifyShow();
+            var delta = e.pageY - lastPageY;
+            lastPageY = e.pageY;
+            raf(function () {
+              content[0].scrollTop += delta / scrollRatio;
+            });
+          }
+
+          function stop() {
+            el.removeClass('ss-grabbed');
+            documentBody.removeClass('ss-grabbed');
+            documentBody.off('mousemove', drag);
+            documentBody.off('mouseup', stop);
+          }
         }
 
-        if (attrs.hasOwnProperty('rebuildOnResize')) {
-          win.on('resize', rebuild);
+        function onMouseEnter(e) {
+          notifyShow();
+          moveBar(e);
         }
+
+        function onMouseLeave(e) {
+          notifyHide();
+        }
+
+        // Constructor
+        function initScrollBar(el) {
+          $timeout(function () {
+            dragDealer(bar);
+            moveBar();
+
+            content.on('scroll', moveBar);
+            el.on('mouseenter', onMouseEnter);
+
+            var css = window.getComputedStyle(el[0]);
+            if (css['height'] === '0px' && css['max-height'] !== '0px') {
+              el.css('height', css['max-height']);
+            }
+          })
+        }
+
+        function moveBar() {
+          var totalHeight = content[0].scrollHeight,
+            ownHeight = container[0].clientHeight;
+
+          scrollRatio = ownHeight / totalHeight;
+          if (ownHeight > 0) {
+            raf(function () {
+              // Hide scrollbar if no scrolling is possible
+              if (scrollRatio >= 1) {
+                bar.addClass('ss-hidden')
+              } else {
+                bar.removeClass('ss-hidden')
+                var top = (content[0].scrollTop / totalHeight) * 100 + '%';
+                bar.css({
+                  height: (scrollRatio) * 100 + '%',
+                  top: top,
+                });
+              }
+            });
+          }
+        }
+
+        initScrollBar(element);
+
       },
-      template: '<div>' +
-      '<div class="ngsb-wrap" ng-mouseenter="showHideScroller($event, true)" ng-mouseleave="showHideScroller($event, false)">' +
-      '<div class="ngsb-container" ng-transclude></div>' +
-      '<div class="ngsb-scrollbar" style="position: absolute; display: block;" ng-show="showYScrollbar">' +
-      '<div class="ngsb-thumb-container">' +
-      '<div class="ngsb-thumb-pos" oncontextmenu="return false;">' +
-      '<div class="ngsb-thumb" ></div>' +
-      '</div>' +
-      '<div class="ngsb-track"></div>' +
-      '</div>' +
-      '</div>' +
-      '</div>' +
-      '</div>'
+      template: '<div class="ss-container" ng-class="{\'show-scroll\': showYScrollbar}"> \
+        <div class="ss-wrapper"> \
+          <div class="ss-content" ng-transclude> \
+          </div> \
+        </div> \
+        <div class="ss-scroll"></div> \
+      </div>',
     };
   }
 ]);
